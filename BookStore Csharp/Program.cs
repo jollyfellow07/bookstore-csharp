04aa39125981281288355b9d06173613cc7ef32a
jollyfellow7 <simonecompagnone7@gmail.com>
2022-04-04 14:26:19 +0200

    Add project files.

---
+++ BookStore Csharp/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string titolo = "La coscienza di Zeno ";
+Console.WriteLine("Titolo: " + titolo + "\n");
+string autore = "Italo Svevo";
+Console.WriteLine("Autore: " + autore + "\n");
+long isbn = 978-8867581252;
+Console.WriteLine("isbn-13: " + isbn + "\n");
+int pagine = 266;
+Console.WriteLine("Pagine: " + pagine + "\n");
+int peso = 580;
+Console.WriteLine("Peso articolo: " + peso + " g\n");
+float lunghezza = 17.78f;
+float spessore = 1.52f;
+float altezza = 25.4f;
+Console.WriteLine("Dimensioni: " + lunghezza+ " x " + spessore + " x " + altezza +  " cm\n");
+
+
+
+
+
+
