96b756be55fe554dcdf5c105f7de09f9257d3fa0
jollyfellow7 <simonecompagnone7@gmail.com>
2022-04-04 16:07:58 +0200

    programma ordinato

--- BookStore Csharp/Program.cs
+++ BookStore Csharp/Program.cs
@@ -1,26 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+//dichiarazione variabili
 string titolo = "La coscienza di Zeno ";
-Console.WriteLine("Titolo: " + titolo + "\n");
 string autore = "Italo Svevo";
-Console.WriteLine("Autore: " + autore + "\n");
 string isbn = "978-8867581252";
-Console.WriteLine("isbn-13: " + isbn + "\n");
 int pagine = 266;
-Console.WriteLine("Pagine: " + pagine + "\n");
 int peso = 580;
-Console.WriteLine("Peso articolo: " + peso + " g\n");
 float lunghezza = 17.78f;
 float spessore = 1.52f;
 float altezza = 25.4f;
-Console.WriteLine("Dimensioni: " + lunghezza + " x " + altezza + " x " + spessore +  " cm\n");
 float valutazione = 4.4f;
-Console.WriteLine("Valutazione media: " + valutazione + " su 5\n");
 int numeroRecensioni = 72;
-Console.WriteLine("Numero di recensioni : " + numeroRecensioni + "\n");
-
 string kindle = "No";
 string copertina = "Si";
+
+//stampo libro
+Console.WriteLine("Titolo: " + titolo + "\n");
+Console.WriteLine("Autore: " + autore + "\n");
+Console.WriteLine("isbn-13: " + isbn + "\n");
+Console.WriteLine("Pagine: " + pagine + "\n");
+Console.WriteLine("Peso articolo: " + peso + " g\n");
+Console.WriteLine("Dimensioni: " + lunghezza + " x " + altezza + " x " + spessore + " cm\n");
+Console.WriteLine("Valutazione media: " + valutazione + " su 5\n");
+Console.WriteLine("Numero di recensioni : " + numeroRecensioni + "\n");
 Console.WriteLine("Disponibilità in kindle: " + kindle + "\n");
 Console.WriteLine("Disponibilità con copertina flessibile : " + copertina);
 
